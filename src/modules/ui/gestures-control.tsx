5a9ac98b026413dc6930ddb10a64abf3b5839f9f
Dmitry Podlesny <rezed93@gmail.com>
2025-02-24 16:32:41 +0700

    🤏🖥️ gestures control hint icon

--- src/modules/ui/gestures-control.tsx
+++ src/modules/ui/gestures-control.tsx
@@ -1,11 +1,19 @@
 import type * as THREE from 'three'
 import { useGesturesControl } from 'modules/hooks/use-gestures-control'
+import { Html } from '@react-three/drei'
+import RotateIconSVG from 'assets/rotate-icon.svg'
 
 type Props = {
-  object: THREE.Object3D
+  object?: THREE.Object3D | null
 }
 
 export function GesturesControl({ object }: Props) {
   useGesturesControl(object)
-  return null
+  return (
+    <Html>
+      <div className={'gestures-rotate-icon'}>
+        <RotateIconSVG />
+      </div>
+    </Html>
+  )
 }
