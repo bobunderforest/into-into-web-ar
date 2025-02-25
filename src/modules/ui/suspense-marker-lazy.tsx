398d911bb15689fc792465b197ecce982e4f7754
Dmitry Podlesny <rezed93@gmail.com>
2025-02-26 03:12:19 +0700

    🪜🎈 suspence-marker-lazy isPreloaded prop

--- src/modules/ui/suspense-marker-lazy.tsx
+++ src/modules/ui/suspense-marker-lazy.tsx
@@ -3,19 +3,20 @@ import { Suspense, useEffect, useState } from 'react'
 import { LoadingAnimation } from './loading-animation'
 
 type Props = {
+  isPreloaded?: boolean
   fallback?: React.ReactNode
   children: React.ReactNode
 }
 
-export function SuspenseMarkerLazy({ fallback, children }: Props) {
+export function SuspenseMarkerLazy({ isPreloaded, fallback, children }: Props) {
   const [wasFoundOnce, setWasFoundOnce] = useState(false)
   const { isFound } = useARMarker()
 
   useEffect(() => {
-    if (!wasFoundOnce && isFound) setWasFoundOnce(true)
-  }, [wasFoundOnce, isFound])
+    if ((!wasFoundOnce && isFound) || isPreloaded) setWasFoundOnce(true)
+  }, [wasFoundOnce, isFound, isPreloaded])
 
-  if (!wasFoundOnce) return
+  if (!wasFoundOnce) return null
 
   return (
     <Suspense fallback={fallback || <LoadingAnimation />}>{children}</Suspense>
